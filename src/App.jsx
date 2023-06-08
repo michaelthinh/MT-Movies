2c81c49822d9ef02dd3212077e397135864a7b62
Michael Thinh <michaelthinh1008@gmail.com>
2023-06-08 09:57:42 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MTLogo from "./assets/img/logo/mt-logo.png";
+
+import "./App.css";
+
+function App() {
+    return (
+        <div className="flex flex-col items-center mt-96">
+            <img src={MTLogo} alt="MT Movies Logo" />
+        </div>
+    );
+}
+
+export default App;
